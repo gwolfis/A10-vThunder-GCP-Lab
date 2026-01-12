37edd14ad9d216354949894455fc3a8673f57dbd
gwolfis <gert.jan.wolfis@gmail.com>
2026-01-12 19:19:43 +0100

    failover update

--- failover/outputs.tf
+++ failover/outputs.tf
@@ -6,6 +6,7 @@ output "failover_vm_internal_ip" {
 
 output "failover_vm_public_ip" {
   value = try(google_compute_instance.failover.network_interface[0].access_config[0].nat_ip, null)
+  depends_on  = [null_resource.wait_for_failover_service]
 }
 
 output "primary_ti_name" {
